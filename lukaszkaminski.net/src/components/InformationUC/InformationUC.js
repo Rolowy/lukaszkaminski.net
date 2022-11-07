6ef53e2deb69b813461799d8d987abda9eb2ecc5
Łukasz Kamiński <rolowaty@gmail.com>
2022-11-07 19:40:24 +0100

    minor fixes

--- lukaszkaminski.net/src/components/InformationUC/InformationUC.js
+++ lukaszkaminski.net/src/components/InformationUC/InformationUC.js
@@ -2,27 +2,30 @@ import React from 'react';
 import styled, {keyframes} from 'styled-components';
 import { pulse } from 'react-animations';
 
+//? --Variables--
 const email = 'kontakt@lukaszkaminski.net';
 const pulse_animation = keyframes`${pulse}`;
 
+
+//? --Styles--
 const Container = styled.div`
+margin: 2em;
 display: flex;
   align-items: center;
   flex-direction: column;
 `
 
-const Icon = styled.h1`
+const Icon = styled.div`
 @import url("https://fonts.googleapis.com/css?family=Russo+One&:400,700&display=swap");
 font-family: 'Russo One', sans-serif;
 margin: 0.5rem;
 font-size: 7rem;
-    text-align: uppercase;
-    color: #ff0000;
-    background: -webkit-linear-gradient(#3c42ed,#2cb1ff);
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: #ffffff00;
+text-align: uppercase;
+color: #ff0000;
+background: -webkit-linear-gradient(#3c42ed,#2cb1ff);
+-webkit-background-clip: text;
+-webkit-text-fill-color: #ffffff00;
     
-
 &:first-letter {
   color: #ddd;
 }
@@ -30,32 +33,29 @@ font-size: 7rem;
 `
 
 const Title = styled.h1`
-
 font-size: 1,5em; 
-animation: 1s ${pulse_animation}
+animation: 1s ${pulse_animation};
+text-align: center;
 `
 
 const Description = styled.a`
 font-size: 1em; 
+text-align: center;
 `
+//? --Styles--
 
-function CropAnimation() {
-  
-}
 
+
+//? --Functions --
 function InformationUc() {
-  let title = document.getElementById('uc-title');
-
-  const getTitle = (() => {return title;});
-
   return (
     <Container>
       <Icon>|ŁK</Icon>
-      <Title id='uc-title'>Website under construction.</Title>
+      <Title>Website under construction.</Title>
       <Description target="blank" href={`mailto:${email}`}>{email}</Description>
     </Container>
   )
 }
-
+//? --Functions --
 
 export default InformationUc;
